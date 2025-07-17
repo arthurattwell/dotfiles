6f7d3c9bd23fb539796859841f64ff42698775a5
Arthur Attwell <arthur@arthurattwell.com>
2025-07-17 16:26:11 +0000

    Add zsh theme

--- .zshrc
+++ .zshrc
@@ -1,3 +1,12 @@
+# Load Oh My Zsh
+export ZSH="$HOME/.oh-my-zsh"
+
+# Set theme
+ZSH_THEME="robbyrussell"
+
+# Load Oh My Zsh
+source $ZSH/oh-my-zsh.sh
+
 # Load bash aliases and functions
 [ -f ~/.aliases ] && source ~/.aliases
 [ -f ~/.functions ] && source ~/.functions
--- install.sh
+++ install.sh
@@ -3,11 +3,16 @@
 # Get the directory where the script is located
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Install Oh My Zsh if not already installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
 # Create symlinks for each dotfile
 ln -sf "$DOTFILES_DIR/.aliases" "$HOME/.aliases"
 ln -sf "$DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
 ln -sf "$DOTFILES_DIR/.bash_profile" "$HOME/.bash_profile"
-ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"  # Add this line
+ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
 
 # Make sure the script is executable
 chmod +x "$DOTFILES_DIR/install.sh"
