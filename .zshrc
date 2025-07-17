3fd874c4d31c9f0f9ee5e28a365725e682bd134a
Arthur Attwell <arthur@arthurattwell.com>
2025-07-17 15:46:54 +0000

    Add zsh support

---
+++ .zshrc
@@ -0,0 +1,7 @@
+# Load bash aliases and functions
+[ -f ~/.aliases ] && source ~/.aliases
+[ -f ~/.functions ] && source ~/.functions
+[ -f ~/.exports ] && source ~/.exports
+[ -f ~/.extra ] && source ~/.extra
+
+# Add any zsh-specific configurations below
--- install.sh
+++ install.sh
@@ -7,11 +7,16 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sf "$DOTFILES_DIR/.aliases" "$HOME/.aliases"
 ln -sf "$DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
 ln -sf "$DOTFILES_DIR/.bash_profile" "$HOME/.bash_profile"
+ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"  # Add this line
 
 # Make sure the script is executable
 chmod +x "$DOTFILES_DIR/install.sh"
 
-# Source the bash profile to apply changes
-source "$HOME/.bash_profile"
+# Source the appropriate profile based on shell
+if [ -n "$ZSH_VERSION" ]; then
+    source "$HOME/.zshrc"
+else
+    source "$HOME/.bash_profile"
+fi
 
 echo "Dotfiles installation complete."
